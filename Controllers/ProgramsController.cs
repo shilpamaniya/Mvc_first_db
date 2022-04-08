cf4d291c11165a203e10f1ce52fbedaaa3b128e8
shilpamaniya <85163108+shilpamaniya@users.noreply.github.com>
2022-04-08 13:35:01 +0530

    Add files via upload

---
+++ Controllers/ProgramsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Db_First;
+
+namespace Mvc_Db_First.Controllers
+{
+    public class ProgramsController : Controller
+    {
+        private Mvc_first_dbEntities db = new Mvc_first_dbEntities();
+
+        // GET: Programs
+        public ActionResult Index(string searchby,string search)
+        {
+            if(searchby == "ProgramId")
+            {
+                int value;
+                if (search == null)
+                {
+                    value = 0;
+                }
+                else 
+                { 
+                    value= Convert.ToInt32(search);
+                }
+                var result = db.Programs.Where(p => p.ProgramId.ToString().
+                Contains(value.ToString()) || value == 0);
+                return View(result.ToList());
+
+                
+            }
+            else if(searchby == "ProgramName")
+            {
+                var result = db.Programs.Where(p => p.ProgramName.Contains(search) || search == null);
+                return View(result.ToList());
+            
+            }
+            return View(db.Programs.ToList());
+        }
+
+        // GET: Programs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        // GET: Programs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Programs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProgramId,ProgramName")] Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Programs.Add(program);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(program);
+        }
+
+        // GET: Programs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        // POST: Programs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProgramId,ProgramName")] Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(program).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(program);
+        }
+
+        // GET: Programs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        // POST: Programs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Program program = db.Programs.Find(id);
+            db.Programs.Remove(program);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_Db_First;
+
+namespace Mvc_Db_First.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private Mvc_first_dbEntities db = new Mvc_first_dbEntities();
+
+        // GET: Students
+        public ActionResult Index(string searchby, string search)
+        {
+            if (searchby == "StudentId")
+            {
+                int value;
+                if (search == null)
+                {
+                    value = 0;
+                }
+                else
+                {
+                    value = Convert.ToInt32(search);
+                }
+                var result = db.Students.Where(s => s.StudentId.ToString().
+                Contains(value.ToString()) || value == 0);
+                return View(result.ToList());
+
+
+            }
+            else if (searchby == "StudentName")
+            {
+                var result = db.Students.Where(s => s.StudentName.Contains(search) || search == null);
+                return View(result.ToList());
+
+            }
+            return View(db.Students.ToList());
+        }
+
+        // GET: Students/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProgramId = new SelectList(db.Programs, "ProgramId", "ProgramName");
+            return View();
+        }
+
+        // POST: Students/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "StudentId,StudentName,Age,DOB,Gender,ProgramId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProgramId = new SelectList(db.Programs, "ProgramId", "ProgramName", student.ProgramId);
+            return View(student);
+        }
+
+        // GET: Students/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProgramId = new SelectList(db.Programs, "ProgramId", "ProgramName", student.ProgramId);
+            return View(student);
+        }
+
+        // POST: Students/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "StudentId,StudentName,Age,DOB,Gender,ProgramId")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProgramId = new SelectList(db.Programs, "ProgramId", "ProgramName", student.ProgramId);
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Student student = db.Students.Find(id);
+            db.Students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
